8bc23e8b73683089293eea02a5567dcc7f61f309
sardorxonakramov <sardorxonakramov01@gmail.com>
2025-02-26 15:20:06 +0500

    Admin panel orqali ham userni qo'shish sozlandi

--- account/admin.py
+++ account/admin.py
@@ -1,13 +1,25 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser
 
-# Register your models here.
-
-class CustomUserAdmin(admin.ModelAdmin):
+class CustomUserAdmin(UserAdmin):  # admin.ModelAdmin O'RNIGA UserAdmin ishlatiladi
     model = CustomUser
-    list_display = ['email', 'first_name', 'last_name', 'age', 'created_at']
-    search_fields = ['email', 'first_name', 'last_name']
-    list_filter = ['age', 'created_at']
-    ordering = ['email']
+    list_display = ("email", "first_name", "last_name", "age", "is_staff", "is_superuser")
+    search_fields = ("email", "first_name", "last_name")
+    ordering = ("email",)
+
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),  # Parol kiritish qismi to'g'ri ishlaydi
+        ("Personal Info", {"fields": ("first_name", "last_name", "age")}),
+        ("Permissions", {"fields": ("is_active", "is_staff", "is_superuser", "groups", "user_permissions")}),
+        ("Important Dates", {"fields": ("last_login", "created_at")}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": ("email", "password1", "password2", "first_name", "last_name", "is_staff", "is_superuser"),
+        }),
+    )
 
 admin.site.register(CustomUser, CustomUserAdmin)
